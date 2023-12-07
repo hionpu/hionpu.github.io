24d058adff603b6bac8f9c61507f2056495ca945
박승우 <b23038.tdc.hanmacgroup@gmail.com>
2023-12-07 11:21:36 +0900

    Runtime errors

--- _site/en/sw.js
+++ _site/en/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701853037';
+const cacheName = 'chirpy-1701915597';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
