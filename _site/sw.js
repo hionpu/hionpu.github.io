c31dc1ea1c2e285d234a65a14f85197f7560e120
박승우 <b23038.tdc.hanmacgroup@gmail.com>
2023-12-07 17:55:54 +0900

    error

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701938407';
+const cacheName = 'chirpy-1701939337';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
