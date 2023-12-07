0362ff5222021ef26e4dacc930c2de6c00f11758
박승우 <b23038.tdc.hanmacgroup@gmail.com>
2023-12-07 11:26:36 +0900

    npm ran

--- _site/en/sw.js
+++ _site/en/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701915597';
+const cacheName = 'chirpy-1701915950';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
