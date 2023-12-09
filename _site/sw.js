09c9ab552a5ce588d2690ddc296f22c899eedd58
hionpu <parkpakr011@gmail.com>
2023-12-09 13:36:43 +0900

    toc offset

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702095226';
+const cacheName = 'chirpy-1702096527';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
