6780c60af7a1483a5764647d44b60f4ab5081d42
hionpu <parkpakr011@gmail.com>
2023-12-09 13:02:17 +0900

    contents

--- _site/en/sw.js
+++ _site/en/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702094147';
+const cacheName = 'chirpy-1702094466';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702094146';
+const cacheName = 'chirpy-1702094465';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
